97b923c202132e04b30b851d9bbdecbc20136a38
Krzysztof Mielcarek <mielcarekkrzysztof0@gmail.com>
2022-10-03 18:26:09 +0200

    Initial commit

---
+++ workout-plan/src/main/java/com/project/workoutplan/WorkoutPlanApplication.java
@@ -0,0 +1,13 @@
+package com.project.workoutplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlanApplication.class, args);
+	}
+
+}
